70b315d88728615c0cdc260ad16e5610f50bda85
alecmariano <alec_mariano@hotmail.com>
2021-09-06 21:05:09 -0300

    developing UI

--- lib/ui/lista_ui.dart
+++ lib/ui/lista_ui.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class ListaUI extends StatelessWidget {
@@ -7,20 +9,71 @@ class ListaUI extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightGreen,
-      body: Container(
-          padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
-          child: Column(
-            children: [
-              CircleAvatar(
-                child: Icon(
-                  Icons.check,
-                  color: Colors.lightGreen,
-                ),
-                backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        backgroundColor: Colors.lightGreen,
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              padding:
+                  EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    child: Icon(
+                      Icons.check,
+                      color: Colors.lightGreen,
+                      size: 50,
+                    ),
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Listinha',
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontFamily: ''),
+                  ),
+                  Text(
+                    '3 Itens',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              )),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('Teste'),
+                    trailing: Checkbox(
+                      value: false,
+                      onChanged: null,
+                    ),
+                  ),
+                ],
               ),
-              Text('Listinha')
-            ],
-          )),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
